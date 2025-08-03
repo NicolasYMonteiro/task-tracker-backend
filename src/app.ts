591d083479d3444345a9a19ddb7213a8a5947d99
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2025-08-03 11:09:02 -0300

    add environment variable in cors configuration

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'https://atlasdesenvolvimento.vercel.app', 
+  origin: `${process.env.CORS_ORIGIN}`, 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   exposedHeaders: ['Set-Cookie'],
