d7376d5da789d0ac5cb04c8bb5f0cd6c0227c019
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2025-08-02 21:09:15 -0300

    refactor: change origin cors

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://atlasdesenvolvimento.vercel.app', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   exposedHeaders: ['Set-Cookie'],
